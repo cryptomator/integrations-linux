eaa880b5e05a533986a2b63de1de1dd137b29d63
Armin Schrenk <armin.schrenk@skymatic.de>
2021-01-26 17:35:47 +0100

    add tests for isSupported() method of two keychain access implementations

---
+++ src/test/java/keychain/KDEWalletKeychainAccessTest.java
@@ -0,0 +1,44 @@
+package keychain;
+
+import org.cryptomator.linux.keychain.KDEWalletKeychainAccess;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Unit tests for KWallet access via DBUS.
+ */
+@EnabledOnOs(OS.LINUX)
+public class KDEWalletKeychainAccessTest {
+
+	private static boolean isInstalled;
+
+	@BeforeAll
+	public static void checkSystemAndSetup() throws IOException {
+		ProcessBuilder dbusSend = new ProcessBuilder("dbus-send","--print-reply","--dest=org.freedesktop.DBus", "/org/freedesktop/DBus", "org.freedesktop.DBus.ListNames");
+		ProcessBuilder grep = new ProcessBuilder("grep", "org.kde.kwallet");
+		try {
+			Process end = ProcessBuilder.startPipeline(List.of(dbusSend, grep)).get(1);
+			if (end.waitFor(1000, TimeUnit.MILLISECONDS)) {
+				isInstalled = end.exitValue() == 0;
+			} else {
+				isInstalled = false;
+			}
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+	}
+
+
+	@Test
+	public void testIsSupported() {
+		KDEWalletKeychainAccess keychainAccess = new KDEWalletKeychainAccess();
+		Assertions.assertEquals(isInstalled, keychainAccess.isSupported());
+	}
+}
---
+++ src/test/java/keychain/SecretServiceKeychainAccessTest.java
@@ -0,0 +1,46 @@
+package keychain;
+
+import org.cryptomator.linux.keychain.SecretServiceKeychainAccess;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Unit tests for GNOME keyring access via DBUS.
+ */
+@EnabledOnOs(OS.LINUX)
+public class SecretServiceKeychainAccessTest {
+
+	private static boolean isInstalled;
+
+	@BeforeAll
+	public static void checkSystemAndSetup() throws IOException {
+		ProcessBuilder dbusSend = new ProcessBuilder("dbus-send","--print-reply","--dest=org.freedesktop.DBus", "/org/freedesktop/DBus", "org.freedesktop.DBus.ListNames");
+		ProcessBuilder grep = new ProcessBuilder("grep", "org.gnome.keyring");
+		try {
+			Process end = ProcessBuilder.startPipeline(List.of(dbusSend, grep)).get(1);
+			if (end.waitFor(1000, TimeUnit.MILLISECONDS)) {
+				isInstalled = end.exitValue() == 0;
+			} else {
+				isInstalled = false;
+			}
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+	}
+
+
+	@Test
+	public void testIsSupported(){
+		SecretServiceKeychainAccess secretService = new SecretServiceKeychainAccess();
+		Assertions.assertEquals(isInstalled, secretService.isSupported());
+	}
+
+
+}
