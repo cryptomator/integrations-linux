491959243a2a4d0f2fc988db741b331d5dc79348
Armin Schrenk <armin.schrenk@skymatic.de>
2023-01-16 14:24:24 +0100

    rename revealpath package

--- src/main/java/org/cryptomator/linux/revealpath/DBusFileMangerRevealPath.java
+++ src/main/java/org/cryptomator/linux/revealpath/DBusFileMangerRevealPath.java
@@ -1,4 +1,4 @@
-package org.cryptomator.linux.revealPaths;
+package org.cryptomator.linux.revealpath;
 
 import org.cryptomator.integrations.revealpath.RevealFailedException;
 import org.cryptomator.integrations.revealpath.RevealPathService;
