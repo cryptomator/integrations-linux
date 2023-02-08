203f9c418ffdfa20742a0235d5e51962d504eb69
Armin Schrenk <armin.schrenk@skymatic.de>
2023-02-08 19:07:14 +0100

    for isSupported() replace "whch" usage by dbus-send: * checking known file manager object paths * compatible with dbus impls returning empty nodes * flatpak compatible

--- src/main/java/org/cryptomator/linux/revealpath/DBusFileMangerRevealPath.java
+++ src/main/java/org/cryptomator/linux/revealpath/DBusFileMangerRevealPath.java
@@ -11,12 +11,15 @@ import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class DBusFileMangerRevealPath implements RevealPathService {
 
+	private static final String[] FILEMANAGER_OBJECT_PATHS = {"/org/gnome/Nautilus", "/org/kde/dolphin", "/xfce/Thunar"};
 	private static final String FOR_FOLDERS = "org.freedesktop.FileManager1.ShowFolders";
 	private static final String FOR_FILES = "org.freedesktop.FileManager1.ShowItems";
 	private static final int TIMEOUT_THRESHOLD = 5000;
@@ -56,19 +59,30 @@ public class DBusFileMangerRevealPath implements RevealPathService {
 
 	@Override
 	public boolean isSupported() {
-		CountDownLatch waitBarrier = new CountDownLatch(3);
-		ProcessBuilder builderExistsDbusSend = new ProcessBuilder().command("which", "dbus-send");
-		ProcessBuilder builderExistsNautilus = new ProcessBuilder().command("which", "nautilus");
-		ProcessBuilder builderExistsDolphin = new ProcessBuilder().command("which", "dolphin");
+		CountDownLatch waitBarrier = new CountDownLatch(FILEMANAGER_OBJECT_PATHS.length + 1);
+		ProcessBuilder dbusSendExistBuilder = new ProcessBuilder().command("test", " `command -v dbus-send`");
+		List<ProcessBuilder> fileManagerExistBuilders = Arrays.stream(FILEMANAGER_OBJECT_PATHS)
+				.map(DBusFileMangerRevealPath::createDbusObjectCheck).toList();
+
 		try {
-			var existsDbusSend = builderExistsDbusSend.start();
+			var existsDbusSend = dbusSendExistBuilder.start();
 			existsDbusSend.onExit().thenRun(waitBarrier::countDown);
-			var existsNautilus = builderExistsNautilus.start();
-			existsNautilus.onExit().thenRun(waitBarrier::countDown);
-			var existsDolphin = builderExistsDolphin.start();
-			existsDolphin.onExit().thenRun(waitBarrier::countDown);
+			//TODO: process process-output in paralell
+			List<Process> fileManagerChecks = fileManagerExistBuilders.stream().map(builder -> {
+				try {
+					return builder.start();
+				} catch (IOException e) {
+					waitBarrier.countDown(); //to prevent blocking
+					return null;
+				}
+			}).filter(Objects::nonNull).toList();
+
+			fileManagerChecks.forEach(process -> process.onExit().thenRun(waitBarrier::countDown));
 			if (waitBarrier.await(TIMEOUT_THRESHOLD, TimeUnit.MILLISECONDS)) {
-				return existsDbusSend.exitValue() == 0 && (existsNautilus.exitValue() == 0 | existsDolphin.exitValue() == 0);
+				var zeroExitfileManagerChecks = fileManagerChecks.stream().filter(p -> p.exitValue() == 0).toList();
+				if (existsDbusSend.exitValue() == 0 && zeroExitfileManagerChecks.size() != 0) {
+					return parseOutputsForActualDBusObject(zeroExitfileManagerChecks);
+				}
 			}
 		} catch (IOException | InterruptedException e) {
 			//NO-OP
@@ -76,4 +90,49 @@ public class DBusFileMangerRevealPath implements RevealPathService {
 		return false;
 	}
 
+	/**
+	 * Parses process stdout to see if dbus-send answer is just an empty node.
+	 * <p>
+	 * Some dbus-send implementations return on calling methods on not-existing objects an empty node
+	 * <pre>
+	 *    &lt;node&gt;
+	 *    &lt;/node&gt;
+	 * </pre>
+	 * instead of throwing an error.
+	 * <p>
+	 * Regarding parsing, see the dbus spec <a href="https://dbus.freedesktop.org/doc/dbus-specification.html#introspection-format">on the introsepction format</a>.
+	 *
+	 * @param dbusChecks List of dbus-send processes with zero-exitcode
+	 * @return if one dbus-send output contains actual content
+	 * @throws IOException if the Inputer reader on the process output cannot be created
+	 */
+	private boolean parseOutputsForActualDBusObject(List<Process> dbusChecks) throws IOException {
+		for (var check : dbusChecks) {
+			try (var reader = check.inputReader(StandardCharsets.UTF_8)) {
+				boolean passedInitialNode = false;
+				var line = reader.readLine();
+				while (line != null) {
+					if (passedInitialNode) {
+						return !line.equals("</node>");
+					}
+					passedInitialNode = line.startsWith("<node"); //root node might also contain name, hence no closing bracket
+					line = reader.readLine();
+				}
+			}
+		}
+		return false;
+	}
+
+	private static ProcessBuilder createDbusObjectCheck(String dbusObjectPath) {
+		return new ProcessBuilder().command(
+				"dbus-send",
+				"--session",
+				"--print-reply",
+				"--reply-timeout=" + TIMEOUT_THRESHOLD,
+				"--dest=org.freedesktop.FileManager1",
+				"--type=method_call",
+				dbusObjectPath,
+				"org.freedesktop.DBus.Introspectable.Introspect"
+		);
+	}
 }
