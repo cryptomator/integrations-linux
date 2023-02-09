6c33f55a9c8f03b3c5e2360367b729506f9f92c0
Armin Schrenk <armin.schrenk@skymatic.de>
2023-02-09 13:20:19 +0100

    only check for FileManager1 interface

--- src/main/java/org/cryptomator/linux/revealpath/DBusSendRevealPathService.java
+++ src/main/java/org/cryptomator/linux/revealpath/DBusSendRevealPathService.java
@@ -1,5 +1,6 @@
 package org.cryptomator.linux.revealpath;
 
+import com.google.common.base.Preconditions;
 import org.cryptomator.integrations.revealpath.RevealFailedException;
 import org.cryptomator.integrations.revealpath.RevealPathService;
 
@@ -11,15 +12,16 @@ import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
+/**
+ * RevealPathService provider using the <a href="https://freedesktop.org/wiki/Specifications/file-manager-interface/">DBus freedesktop FileManager1 interface</a> and dbus-send command.
+ */
 public class DBusSendRevealPathService implements RevealPathService {
 
-	private static final String[] FILEMANAGER_OBJECT_PATHS = {"/org/gnome/Nautilus", "/org/kde/dolphin", "/org/xfce/Thunar"};
+	private static final String FILEMANAGER1_XML_ELEMENT = "<interface name=\"org.freedesktop.FileManager1\">";
 	private static final String FOR_FOLDERS = "org.freedesktop.FileManager1.ShowFolders";
 	private static final String FOR_FILES = "org.freedesktop.FileManager1.ShowItems";
 	private static final int TIMEOUT_THRESHOLD = 5000;
@@ -59,29 +61,19 @@ public class DBusSendRevealPathService implements RevealPathService {
 
 	@Override
 	public boolean isSupported() {
-		CountDownLatch waitBarrier = new CountDownLatch(FILEMANAGER_OBJECT_PATHS.length + 1);
-		ProcessBuilder dbusSendExistBuilder = new ProcessBuilder().command("test", " `command -v dbus-send`");
-		List<ProcessBuilder> fileManagerExistBuilders = Arrays.stream(FILEMANAGER_OBJECT_PATHS)
-				.map(DBusSendRevealPathService::createDbusObjectCheck).toList();
+		CountDownLatch waitBarrier = new CountDownLatch(2);
+		ProcessBuilder dbusSendExistsBuilder = new ProcessBuilder().command("test", " `command -v dbus-send`");
+		ProcessBuilder fileManager1ExistsBuilder = createFileManager1Check();
 
 		try {
-			var existsDbusSend = dbusSendExistBuilder.start();
-			existsDbusSend.onExit().thenRun(waitBarrier::countDown);
-			//TODO: process process-output in paralell
-			List<Process> fileManagerChecks = fileManagerExistBuilders.stream().map(builder -> {
-				try {
-					return builder.start();
-				} catch (IOException e) {
-					waitBarrier.countDown(); //to prevent blocking
-					return null;
-				}
-			}).filter(Objects::nonNull).toList();
+			var dbusSendExists = dbusSendExistsBuilder.start();
+			dbusSendExists.onExit().thenRun(waitBarrier::countDown);
+			var fileManager1Exists = fileManager1ExistsBuilder.start();
+			fileManager1Exists.onExit().thenRun(waitBarrier::countDown);
 
-			fileManagerChecks.forEach(process -> process.onExit().thenRun(waitBarrier::countDown));
 			if (waitBarrier.await(TIMEOUT_THRESHOLD, TimeUnit.MILLISECONDS)) {
-				var zeroExitfileManagerChecks = fileManagerChecks.stream().filter(p -> p.exitValue() == 0).toList();
-				if (existsDbusSend.exitValue() == 0 && zeroExitfileManagerChecks.size() != 0) {
-					return parseOutputsForActualDBusObject(zeroExitfileManagerChecks);
+				if (dbusSendExists.exitValue() == 0 && fileManager1Exists.exitValue() == 0) {
+					return parseOutputForFileManagerInterface(fileManager1Exists);
 				}
 			}
 		} catch (IOException | InterruptedException e) {
@@ -91,39 +83,21 @@ public class DBusSendRevealPathService implements RevealPathService {
 	}
 
 	/**
-	 * Parses process stdout to see if dbus-send answer is just an empty node.
-	 * <p>
-	 * Some dbus-send implementations return on calling methods on not-existing objects an empty node
-	 * <pre>
-	 *    &lt;node&gt;
-	 *    &lt;/node&gt;
-	 * </pre>
-	 * instead of throwing an error.
-	 * <p>
-	 * Regarding parsing, see the dbus spec <a href="https://dbus.freedesktop.org/doc/dbus-specification.html#introspection-format">on the introsepction format</a>.
+	 * Parses process stdout to see if the answer contains "{@value FILEMANAGER1_XML_ELEMENT}".
+	 * DBus introspection output is defined in the <a href="https://dbus.freedesktop.org/doc/dbus-specification.html#introspection-format">dbus spec</a>.
 	 *
-	 * @param dbusChecks List of dbus-send processes with zero-exitcode
-	 * @return if one dbus-send output contains actual content
+	 * @param fileManager1Process The already exited process for checking the FileManager1 interface
+	 * @return {@code true} if the interface is found in the introspection output, otherwise false
 	 * @throws IOException if the Inputer reader on the process output cannot be created
 	 */
-	private boolean parseOutputsForActualDBusObject(List<Process> dbusChecks) throws IOException {
-		for (var check : dbusChecks) {
-			try (var reader = check.inputReader(StandardCharsets.UTF_8)) {
-				boolean passedInitialNode = false;
-				var line = reader.readLine();
-				while (line != null) {
-					if (passedInitialNode) {
-						return !line.equals("</node>");
-					}
-					passedInitialNode = line.startsWith("<node"); //root node might also contain name, hence no closing bracket
-					line = reader.readLine();
-				}
-			}
+	private boolean parseOutputForFileManagerInterface(Process fileManager1Process) throws IOException {
+		Preconditions.checkState(!fileManager1Process.isAlive());
+		try (var reader = fileManager1Process.inputReader(StandardCharsets.UTF_8)) {
+			return reader.lines().map(String::trim).anyMatch(FILEMANAGER1_XML_ELEMENT::equals);
 		}
-		return false;
 	}
 
-	private static ProcessBuilder createDbusObjectCheck(String dbusObjectPath) {
+	private static ProcessBuilder createFileManager1Check() {
 		return new ProcessBuilder().command(
 				"dbus-send",
 				"--session",
@@ -131,7 +105,7 @@ public class DBusSendRevealPathService implements RevealPathService {
 				"--reply-timeout=" + TIMEOUT_THRESHOLD,
 				"--dest=org.freedesktop.FileManager1",
 				"--type=method_call",
-				dbusObjectPath,
+				"/org/freedesktop/FileManager1",
 				"org.freedesktop.DBus.Introspectable.Introspect"
 		);
 	}
