5163d8b25753a06db48521c593e6d0c4e46c5dbc
Ralph Plawetzki <ralph@purejava.org>
2025-10-26 10:14:53 +0100

    Ensure unlocking Add handlers

--- src/main/java/org/cryptomator/linux/keychain/SecretServiceKeychainAccess.java
+++ src/main/java/org/cryptomator/linux/keychain/SecretServiceKeychainAccess.java
@@ -10,7 +10,11 @@ import org.purejava.secret.api.Collection;
 import org.purejava.secret.api.EncryptedSession;
 import org.purejava.secret.api.Item;
 import org.purejava.secret.api.Static;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Priority(900)
@@ -18,8 +22,18 @@ import java.util.Map;
 @DisplayName("Secret Service")
 public class SecretServiceKeychainAccess implements KeychainAccessProvider {
 
+	private static final Logger LOG = LoggerFactory.getLogger(SecretServiceKeychainAccess.class);
 	private final EncryptedSession session = new EncryptedSession();
 	private final Collection collection = new Collection(new DBusPath(Static.DBusPath.DEFAULT_COLLECTION));
+
+	public SecretServiceKeychainAccess() {
+		session.getService().addCollectionChangedHandler(collection -> LOG.debug("Collection {} changed", collection.getPath()));
+		session.getService().addCollectionCreatedHandler(collection -> LOG.debug("Collection {} created", collection.getPath()));
+		session.getService().addCollectionDeletedHandler(collection -> LOG.debug("Collection {} deleted", collection.getPath()));
+		collection.addItemChangedHandler(item -> LOG.debug("Item {} changed", item.getPath()));
+		collection.addItemCreatedHandler(item -> LOG.debug("Item {} created", item.getPath()));
+		collection.addItemDeletedHandler(item -> LOG.debug("Item {} deleted", item.getPath()));
+	}
 
 	@Override
 	public void storePassphrase(String key, String displayName, CharSequence passphrase) throws KeychainAccessException {
@@ -27,6 +41,9 @@ public class SecretServiceKeychainAccess implements KeychainAccessProvider {
 			var call = collection.searchItems(createAttributes(key));
 			if (call.isSuccess()) {
 				if (call.value().isEmpty()) {
+					List<DBusPath> lockable = new ArrayList<>();
+					lockable.add(new DBusPath(collection.getDBusPath()));
+					session.getService().unlock(lockable);
 					var itemProps = Item.createProperties(displayName, createAttributes(key));
 					var secret = session.encrypt(passphrase);
 					var created = collection.createItem(itemProps, secret, false);
@@ -51,6 +68,7 @@ public class SecretServiceKeychainAccess implements KeychainAccessProvider {
 			if (call.isSuccess()) {
 				if (!call.value().isEmpty()) {
 					var path = call.value().getFirst();
+					session.getService().ensureUnlocked(path);
 					var secret = new Item(path).getSecret(session.getSession());
 					return session.decrypt(secret);
 				} else {
@@ -71,6 +89,7 @@ public class SecretServiceKeychainAccess implements KeychainAccessProvider {
 			if (call.isSuccess()) {
 				if (!call.value().isEmpty()) {
 					var path = call.value().getFirst();
+					session.getService().ensureUnlocked(path);
 					var item = new Item(path);
 					var deleted = item.delete();
 					if (!deleted.isSuccess()) {
@@ -94,6 +113,7 @@ public class SecretServiceKeychainAccess implements KeychainAccessProvider {
 			var call = collection.searchItems(createAttributes(key));
 			if (call.isSuccess()) {
 				if (!call.value().isEmpty()) {
+					session.getService().ensureUnlocked(call.value().getFirst());
 					var secret = session.encrypt(passphrase);
 					var itemProps = Item.createProperties(displayName, createAttributes(key));
 					var updated = collection.createItem(itemProps, secret, true);
